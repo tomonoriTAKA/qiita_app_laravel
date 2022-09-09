c96e015cbd37e5e2613a8076b019daba319ed863
Tomonori <tomonori0204.takahashi@gmail.com>
2022-09-09 23:43:59 +0900

    詳細画面の途中まで

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class ArticleController extends Controller
 {
@@ -13,7 +14,33 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $method = 'GET';
+        $tag_id = 'PHP';
+        $per_page = 30;
+
+        // QIITA_URLの値を取得してURLを定義
+        $url = config('qiita.url') . '/api/v2/tags/' . $tag_id . '/items?per_page=' . $per_page;
+        // $optionsにトークンを指定
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+            ],
+        ];
+
+        // Client(接続する為のクラス)を生成
+        $client = new Client();
+
+        // try catchでエラー時の処理を書く
+        try {
+            // データを取得し、JSON形式からPHPの変数に変換
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $articles = json_decode($body, false);
+        } catch (\Throwable $th) {
+            $articles = null;
+        }
+
+        return view('articles.index')->with(compact('articles'));
     }
 
     /**
@@ -45,7 +72,40 @@ class ArticleController extends Controller
      */
     public function show($id)
     {
-        //
+        $method = 'GET';
+
+        // QIITA_URLの値を取得してURLを定義
+        $url = config('qiita.url') . '/api/v2/items/' . $id;
+
+        // Client(接続する為のクラス)を生成
+        $client = new Client();
+
+        // $optionsにトークンを指定
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+            ],
+        ];
+
+        try {
+            // データを取得し、JSON形式からPHPの変数に変換
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $article = json_decode($body, false);
+
+            // 変換するクラスをインスタンス化して設定を追加
+            $parser = new \cebe\markdown\GithubMarkdown();
+            $parser->keepListStartNumber = true;  // olタグの番号の初期化を有効にする
+            $parser->enableNewlines = true;  // 改行を有効にする
+
+            // MarkdownをHTML文字列に変換し、HTMLに変換(エスケープする)
+            $html_string = $parser->parse($article->body);
+            $article->html = new \Illuminate\Support\HtmlString($html_string);
+        } catch (\Throwable $th) {
+            return back();
+        }
+
+        return view('articles.show')->with(compact('article'));
     }
 
     /**
---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@3.0.1/github-markdown.min.css">
+</head>
+
+<body>
+    <button type="button" onclick="location.href='{{ route('articles.index') }}'">一覧へ戻る</button>
+    <h1>{{ $article->title }}</h1>
+    <div class="markdown-body">
+        {{ $article->html }}
+    </div>
+</body>
+
+</html>
